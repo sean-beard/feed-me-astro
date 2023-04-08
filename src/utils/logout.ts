32ab0aa68e32596b4a6001f2fab16bfaaf6fe34f
Sean Beard <sean61793@my.uri.edu>
2023-04-07 22:39:00 -0600

    feat: add mobile nav

---
+++ src/utils/logout.ts
@@ -0,0 +1,15 @@
+import Cookies from "js-cookie";
+
+export const logout = async () => {
+  const response = await fetch(
+    `${import.meta.env.PUBLIC_BASE_API_URL}/auth/logout`
+  );
+  const data = await response.json();
+
+  if (data.status === 200) {
+    Cookies.remove("user");
+    window.location.replace("/");
+  }
+
+  // TODO: handle error
+};
