e94cc2a81870cef17f6d13edc5b99b77b5cdc596
Sean Beard <sean61793@my.uri.edu>
2023-11-05 17:18:32 -0700

    test: improve cypress setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     env: {
-      apiBaseUrl: "http://localhost:4000",
+      apiBaseUrl: "http://0.0.0.0:4000",
+      // apiBaseUrl: "http://localhost:4000",
     },
     baseUrl: "http://localhost:8080",
     setupNodeEvents(on, config) {
